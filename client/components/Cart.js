c087c1638f24396e6d8afa2f23e9f32ee4b4e8a4
Rachel Taverna <rtaverna96@gmail.com>
2020-12-16 12:25:53 -0500

    Cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,41 +1,33 @@
 import React from 'react'
-import {connect} from 'react-redux'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-import {getItems} from '../store/item'
-import {addToCart} from '../store/item'
-// import img1 from './IMG_0449.jpeg';
-// import img2 from './IMG_0452.jpeg';
-// import img3 from './IMG_0453.jpeg';
-// import img4 from './IMG_0454.jpeg';
-import store from '../store'
-// import Cart from './Cart';
-// import PropTypes from "prop-types";
-// import { withRouter } from "react-router";
+import {connect} from 'react-redux'
+import store from '../store/index'
+import {getCartItems} from '../store/index'
+import {removeCartItem} from '../store/index'
 
-class All extends React.Component {
+class Cart extends React.Component {
   constructor() {
     super()
-
     this.state = store.getState()
     this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
-    this.props.getItems()
+    this.props.getCartItems()
   }
 
   handleClick(e) {
-    console.log(e.target.value)
-    e.target.disabled = true
-    this.props.addToCart(e.target.value)
+    e.preventDefault()
+    this.props.removeCartItem(e.target.value)
   }
 
   render() {
-    return (
-      <div>
-        <h2>All Beads</h2>
+    let items = this.props.items
 
+    return (
+      <div id="cart">
+        <h2>Your Cart:</h2>
         <div id="all-beads">
           {this.props.items.map(item => (
             <Card key={item.id}>
@@ -48,7 +40,7 @@ class All extends React.Component {
                   value={item.id}
                   onClick={this.handleClick}
                 >
-                  Add to Cart
+                  Remove
                 </Button>
               </Card.Body>
             </Card>
@@ -58,14 +50,15 @@ class All extends React.Component {
     )
   }
 }
-
 const mapStateToProps = state => ({
   items: state.item
 })
 
-const mapDispatchToProps = dispatch => ({
-  getItems: () => dispatch(getItems()),
-  addToCart: id => dispatch(addToCart(id))
-})
+const mapDispatchToProps = dispatch => {
+  return {
+    getCartItems: () => dispatch(getCartItems()),
+    removeCartItem: id => dispatch(removeCartItem(id))
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(All)
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
